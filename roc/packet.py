73ca47ba98401a23a6b6798a502f6066e64a9d6a
李铭昕 <715557344@qq.com>
2024-11-21 16:12:53 +0800

    init

---
+++ roc/packet.py
@@ -0,0 +1,11 @@
+PING = 'ping'
+PONG = 'pong'
+
+
+class Packet:
+    def __init__(self, id: int = 0, body: str = ""):
+        self.id = id
+        self.body = body
+
+    def is_heartbeat(self):
+        return self.id == 0 and (self.body == PING or self.body == PONG)
---
+++ tests/main.py
@@ -0,0 +1,9 @@
+from roc.id_generator import IdGenerator
+
+ret = IdGenerator()
+
+print(ret.generate())
+
+print(ret.generate())
+
+print(ret.generate())
